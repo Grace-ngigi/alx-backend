bb5de73640df0fcd2b1ae036c2821750b1a927f7
Grace-ngigi <gracengigi859@gmail.com>
2024-01-03 05:14:33 +0300

    fifo caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,6 +5,8 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    ''' Fifo Caching '''
+
     def __init__(self):
         ''' Init Fifo'''
         super().__init__()
@@ -16,7 +18,7 @@ class FIFOCache(BaseCaching):
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
                 print(f"DISCARD: {self.order_list[0]}") 
                 del self.cache_data[self.order_list[0]]
-                del self.order_list[0]
+                self.order_list.pop(0)
             self.order_list.append(key)    
             self.cache_data[key] = item
 
