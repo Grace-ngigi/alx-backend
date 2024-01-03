864620bc549d0e99b676e4c7f4eee088dcccca23
Grace_ngigi <gracengigi859@gmail.com>
2024-01-02 18:22:20 -0800

    fifo caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,14 +15,15 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         ''' add item to cache'''
         if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print(f"DISCARD: {self.order_list[0]}") 
+            leng = len(self.cache_data)
+            capacity = BaseCaching.MAX_ITEMS
+            if leng >= capacity and key not in self.cache_data:
+                print(f"DISCARD: {self.order_list[0]}")
                 del self.cache_data[self.order_list[0]]
                 self.order_list.pop(0)
-            self.order_list.append(key)    
+            self.order_list.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
         ''' get item from cache'''
         return self.cache_data.get(key, None)
-    
