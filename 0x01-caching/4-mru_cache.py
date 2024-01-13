84f51a338c17f5d37d5380b939665f864fed9053
Grace-ngigi <gracengigi859@gmail.com>
2024-01-13 14:26:12 +0300

    MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+''' MRU Caching '''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    ''' MRU Caching '''
+
+    def __init__(self):
+        ''' Init MRU'''
+        super().__init__()
+        self.order_list = []
+
+    def put(self, key, item):
+        ''' add item to cache'''
+        if key is not None and item is not None:
+            leng = len(self.cache_data)
+            capacity = BaseCaching.MAX_ITEMS
+            if leng >= capacity and key not in self.cache_data:
+                print(f"DISCARD: {self.order_list[-1]}")
+                del self.cache_data[self.order_list[-1]]
+                del self.order_list[-1]
+            if key in self.order_list:
+                del self.order_list[self.order_list.index(key)]
+            self.order_list.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        ''' get item from cache'''
+        if key is not None and key in self.cache_data.keys():
+            del self.order_list[self.order_list.index(key)]
+            self.order_list.append(key)
+            return self.cache_data[key]
+        return None
