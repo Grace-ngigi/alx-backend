a9ef7c2d722e731c113a7758d1bb6a36eb9d165b
Grace-ngigi <gracengigi859@gmail.com>
2024-01-13 14:11:54 +0300

    LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+''' LRU Caching '''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    ''' LRU Caching '''
+
+    def __init__(self):
+        ''' Init LRU'''
+        super().__init__()
+        self.order_list = []
+
+    def put(self, key, item):
+        ''' add item to cache'''
+        if key is not None and item is not None:
+            leng = len(self.cache_data)
+            capacity = BaseCaching.MAX_ITEMS
+            if leng >= capacity and key not in self.cache_data:
+                print(f"DISCARD: {self.order_list[0]}")
+                del self.cache_data[self.order_list[0]]
+                del self.order_list[0]
+            if key in self.order_list:
+                del self.order_list[self.order_list.index(key)]
+            self.order_list.append(key)   
+            self.cache_data[key] = item
+
+    def get(self, key):
+        ''' get item from cache'''
+        return self.cache_data.get(key, None)
