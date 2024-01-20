3d5f7de62b3457f9452e74ee62c09aa049c0030d
Grace_ngigi <gracengigi859@gmail.com>
2024-01-20 06:05:07 -0800

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -21,6 +21,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    ''' Get locale from request '''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
