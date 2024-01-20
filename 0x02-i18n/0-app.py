72c32a2eda24fc76b5a23b91c3905d388973526b
Grace-ngigi <gracengigi859@gmail.com>
2024-01-20 16:16:11 +0300

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,8 +5,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def ola():
+def hola() -> str:
+    ''' basic flask app '''
     return render_template('template/index.html')
 
 if __name__ == '__main__':
-    app.run("0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
