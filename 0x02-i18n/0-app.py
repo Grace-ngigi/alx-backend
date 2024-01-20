c1e8f8ec68221f43111ee8dcf284c7b95c1124ad
Grace_ngigi <gracengigi859@gmail.com>
2024-01-20 05:19:38 -0800

    corection

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,10 +4,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hola() -> str:
     ''' basic flask app '''
-    return render_template('template/index.html')
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
